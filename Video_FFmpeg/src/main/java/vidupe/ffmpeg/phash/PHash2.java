c7d5e64c5af953985f9744788ec4c4fc3a9c8ac7
farheen <farheen.sk@gmail.com>
2018-03-02 23:24:00 -0800

    Updated PHash2

--- Video_FFmpeg/src/main/java/vidupe/ffmpeg/phash/PHash2.java
+++ Video_FFmpeg/src/main/java/vidupe/ffmpeg/phash/PHash2.java
@@ -1,12 +1,12 @@
 package vidupe.ffmpeg.phash;
 
-import org.opencv.core.Core;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorConvertOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -26,16 +26,12 @@ public class PHash2 {
     private int size = 32;
 
     private int smallerSize = 8;
-    private ColorConvertOp colorConvert = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
-    private double[] c;
 
     public PHash2() {
 
         initCoefficients();
 
     }
-
-// Returns a 'binary string' (like. 001010111011100010) which is easy to do a hamming distance on.
 
     public PHash2(int size, int smallerSize) {
 
@@ -45,38 +41,6 @@ public class PHash2 {
 
         initCoefficients();
 
-    }
-
-    private static int getRed(BufferedImage img, int x, int y) {
-        int rgb = img.getRGB(x, y);
-        return (rgb >> 16) & 0xff;
-    }
-
-    private static int getGreen(BufferedImage img, int x, int y) {
-        int rgb = img.getRGB(x, y);
-        return (rgb >> 8) & 0xff;
-    }
-
-    private static int getBlue(BufferedImage img, int x, int y) {
-        return (img.getRGB(x, y)) & 0xff;
-    }
-
-    public static void main(String args[]) {
-       System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        PHash2 p = new PHash2();
-        String image1;
-        String image2;
-        String path1 = "/media/farheen/01D26F1D020D3380/sample/similarityTest/39/";
-        String path2 = "/media/farheen/01D26F1D020D3380/sample/similarityTest/53-37/";
-        try {
-            image1 = p.getHash(new FileInputStream(new File(path1 + "390001.jpg")));
-
-            image2 = p.getHash(new FileInputStream(new File(path2 + "53-370001.jpg")));
-            System.out.println(image1 + " " + image2);
-            System.out.println("1:2 Score is " + p.distance(image1, image2));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public int distance(String s1, String s2) {
@@ -95,6 +59,71 @@ public class PHash2 {
 
         return counter;
 
+    }
+
+// Returns a 'binary string' (like. 001010111011100010) which is easy to do a hamming distance on.
+
+    private ColorConvertOp colorConvert = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
+    private double[] c;
+
+    private BufferedImage resize(BufferedImage image, int width, int height) {
+        BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(image, 0, 0, width, height, null);
+        g.dispose();
+        return resizedImage;
+    }
+
+    private static int getRed(BufferedImage img, int x, int y) {
+        int rgb = img.getRGB(x, y);
+        return (rgb >> 16) & 0xff;
+    }
+
+    private BufferedImage grayscale(BufferedImage img) {
+        colorConvert.filter(img, img);
+        return img;
+    }
+
+    private BufferedImage toYCrCb(BufferedImage img, int width, int height){
+        BufferedImage ycb = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        int red = getRed(img, width, height);
+        int green = getGreen(img, width, height);
+        int blue = getBlue(img, width, height);
+        int y = (int)(0.299* red + 0.589* green + 0.114* blue);
+        int cb = (int)(128-0.169*red - 0.331*green + 0.500*blue);
+        int cr = (int)(128+0.500*red - 0.419*green - 0.081*blue);
+
+
+        int val = (y<<16) | (cb<<8) | cr;
+        ycb.setRGB(0, 0 ,val);
+        System.out.println(red);
+        return ycb;
+    }
+
+    private static int getGreen(BufferedImage img, int x, int y) {
+        int rgb = img.getRGB(x, y);
+        return (rgb >> 8) & 0xff;
+    }
+
+    private static int getBlue(BufferedImage img, int x, int y) {
+        return (img.getRGB(x, y)) & 0xff;
+    }
+
+    public static void main(String args[]) {
+        PHash2 p = new PHash2();
+        String image1;
+        String image2;
+        String path1 = "/media/farheen/01D26F1D020D3380/sample/similarityTest/39/";
+        String path2 = "/media/farheen/01D26F1D020D3380/sample/similarityTest/53-37/";
+        try {
+            image1 = p.getHash(new FileInputStream(new File(path1 + "390001.jpg")));
+
+            image2 = p.getHash(new FileInputStream(new File(path2 + "53-370001.jpg")));
+            System.out.println(image1 + " " + image2);
+            System.out.println("1:2 Score is " + p.distance(image1, image2));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public String getHash(InputStream is) throws Exception {
@@ -122,10 +151,10 @@ public class PHash2 {
          * the number of computations.
 
          */
-        img = resize(img, size, size);
-        img = grayscale(img);
-      //  img = getConvolutedImage(img);
 
+        img = grayscale(img);
+        img = getConvolutedImage(img);
+        img = resize(img, size, size);
         double[][] vals = new double[size][size];
 
         for (int x = 0; x < img.getWidth(); x++) {
@@ -135,8 +164,8 @@ public class PHash2 {
 
                 //vals[x][y] = (getRed(img, x, y) * 299 / 1000) + (getGreen(img, x, y) * 587 / 1000) + (getBlue(img, x, y) * 114 / 1000);
 
-               // vals[x][y] = getRed(img,x,y);
-               // vals[x][y] = getGreen(img,x,y);
+                // vals[x][y] = getRed(img,x,y);
+                // vals[x][y] = getGreen(img,x,y);
                 vals[x][y] = getBlue(img,x,y);
             }
 
@@ -226,41 +255,6 @@ public class PHash2 {
         return hash;
     }
 
-    private BufferedImage getConvolutedImage(BufferedImage img) throws IOException {
-        OpenCVConvolute openCVConvolute = new OpenCVConvolute();
-        BufferedImage convolute = openCVConvolute.convolute(img);
-        return convolute;
-    }
-
-    private BufferedImage resize(BufferedImage image, int width, int height) {
-        BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = resizedImage.createGraphics();
-        g.drawImage(image, 0, 0, width, height, null);
-        g.dispose();
-        return resizedImage;
-    }
-
-    private BufferedImage grayscale(BufferedImage img) {
-        colorConvert.filter(img, img);
-        return img;
-    }
-
-    private BufferedImage toYCrCb(BufferedImage img, int width, int height){
-        BufferedImage ycb = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        int red = getRed(img, width, height);
-        int green = getGreen(img, width, height);
-        int blue = getBlue(img, width, height);
-        int y = (int)(0.299* red + 0.589* green + 0.114* blue);
-        int cb = (int)(128-0.169*red - 0.331*green + 0.500*blue);
-        int cr = (int)(128+0.500*red - 0.419*green - 0.081*blue);
-
-
-        int val = (y<<16) | (cb<<8) | cr;
-        ycb.setRGB(0, 0 ,val);
-        System.out.println(red);
-        return ycb;
-    }
-
     private void initCoefficients() {
         c = new double[size];
 
@@ -287,5 +281,18 @@ public class PHash2 {
             }
         }
         return F;
+    }
+
+    private BufferedImage getConvolutedImage(BufferedImage img) throws IOException {
+        BufferedImage dstImage = null;
+        float[] sharpen = new float[] {
+                0.1f, 0.1f, 0.1f,
+                0.1f, 0.1f, 0.1f,
+                0.1f, 0.1f, 0.1f
+        };
+        Kernel kernel = new Kernel(3, 3, sharpen);
+        ConvolveOp op = new ConvolveOp(kernel);
+        dstImage = op.filter(img, null);
+        return dstImage;
     }
 }
