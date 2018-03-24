ca9fc77e3218362fc6814ac58ad318492b11abc9
farheen <farheen.sk@gmail.com>
2018-03-23 22:07:53 -0700

    Add missing VideoHashesInformation

---
+++ Frontend/src/main/java/vidupe/frontend/VideoHashesInformation.java
@@ -0,0 +1,18 @@
+package vidupe.frontend;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+@JsonIgnoreProperties("hashes")
+public class VideoHashesInformation {
+    String videoID;
+    String videoName;
+    long duration;
+    List<List<String>> hashes;
+
+}
