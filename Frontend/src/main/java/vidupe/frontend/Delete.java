d32cead3212ca7e79b6e1d95ecfb84e237f16bb1
farheen <farheen.sk@gmail.com>
2018-03-23 22:06:59 -0700

    Add front-end files to display results

---
+++ Frontend/src/main/java/vidupe/frontend/Delete.java
@@ -0,0 +1,75 @@
+package vidupe.frontend;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.cloud.datastore.*;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+@WebServlet("/delete")
+public class Delete extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String jobId = request.getParameter("jobid");
+        String email = request.getParameter("email");
+        System.out.println(jobId+"   "+email);
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println(
+                "<html>"+
+                        "<head>"+
+                        "<title>Results</title>" +
+                        "<link rel='stylesheet' href='style.css'>"+
+                        "</head>"+
+                        "<body>" +
+                        "<div class='background-top'>" +
+                        "<h1 class='displayText'>ViDupe- Duplicate Video Detection Service</h1>" +
+                        "</div>"+
+                        "<div class='display-section'>"
+        );
+        if(request.getParameterValues("video_array")!=null){
+            String [] result = request.getParameterValues("video_array");
+            if(result.length>0){
+                final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
+                Datastore datastore = DatastoreOptions.newBuilder().setNamespace("vidupe").build().getService();
+                Key key = datastore.newKeyFactory()
+                        .setKind("tokens")
+                        .addAncestors(PathElement.of("user", email))
+                        .newKey(jobId);
+                Entity entity= datastore.get(key);
+                String accessToken=entity.getString("accessToken");
+                GoogleCredential credential = new GoogleCredential().setAccessToken(accessToken).createScoped(SCOPES);
+
+                Drive drive = new Drive.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance(), credential)
+                        .setApplicationName("Duplicate video Detection").build();
+
+                for(String fileId:result){
+                    try {
+                        drive.files().delete(fileId).execute();
+                    } catch (IOException e) {
+                        out.println("<p> Error in deleting the file </p>");
+                    }
+                }
+                out.println("<p>Successfully deleted video files</p>");
+            }
+        }
+
+        else{
+            out.println("<p>No files to delete </p>");
+        }
+        out.println("</div></body>");
+        out.println("</html>");
+        out.close();
+
+    }
+}
---
+++ Frontend/src/main/java/vidupe/frontend/Results.java
@@ -0,0 +1,95 @@
+package vidupe.frontend;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.protobuf.ByteString;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.util.List;
+
+
+@WebServlet("/Results")
+public class Results extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        String jobId = request.getParameter("jobid");
+        String email = request.getParameter("email");
+        System.out.println(jobId + "  " + email);
+        String storageURL = "https://storage.cloud.google.com/vidupe/";
+        String encodedEmail = URLEncoder.encode(email, "UTF-8");
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        Blob blob = storage.get("vidupe", email + "/" + jobId);
+        boolean isServletNeedRefresh = true;
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println(
+                "<html>" +
+                        "<head>" +
+                        "<title>Results</title>" +
+                        "<link rel='stylesheet' href='style.css'>" +
+                        "</head>" +
+                        "<body>" +
+                        "<div class='background-top'>" +
+                        "<h1 class='displayText'>ViDupe- Duplicate Video Detection Service</h1>" +
+                        "</div>" +
+                        "<div>"
+
+        );
+        if (blob != null && blob.exists()) {
+            isServletNeedRefresh = false;
+            out.println("<a style='width:400px;margin:auto;padding-top:30px;' href='https://mail.google.com/mail/u/0/?logout&hl=en'>");
+            out.println("<button class='loginBtn loginBtn--google' style='float:right;'>Sign Out</button></a>" +
+                    "</div><div>");
+            out.println("<form action='" + request.getContextPath() + "/delete?email=" + email + "&jobid=" + jobId + "' method='get'>");
+            out.println("<input type='hidden' name='email' value='" + email + "'/>");
+            out.println("<input type='hidden' name='jobid' value='" + jobId + "'/>");
+
+            out.println("<table>");
+            out.println("<tbody>");
+            ByteString content = ByteString.copyFrom(blob.getContent());
+            String messageString = content.toStringUtf8();
+            ObjectMapper mapper = new ObjectMapper();
+            DuplicateVideosList duplicateVideosList = mapper.readValue(messageString, DuplicateVideosList.class);
+            if (duplicateVideosList != null && duplicateVideosList.getDuplicateVideosList().size() > 0) {
+                for (List<VideoHashesInformation> duplicateGroup : duplicateVideosList.getDuplicateVideosList()) {
+                    out.println("<tr style='max-height: 150px; max-width: 150px'>" +
+                            "<td>Duplicates Set</td>");
+                    for (VideoHashesInformation video : duplicateGroup) {
+                        String url = "https://drive.google.com/thumbnail?id=" + video.getVideoID();
+                        out.println("<td>");
+                        out.println("<img src='" + url + "'style='max-height: 150px; max-width: 150px;'></img></br>" +
+                                "<input type='checkbox' name='video_array' value='" + video.getVideoID() + "'>" + video.getVideoName() + "</input></td>");
+                    }
+                    out.println("</tr>");
+                }
+                out.println("</tbody></table>" +
+                        "<input type='submit' name='submit' value='permanently delete videos'>" +
+                        "</input>" +
+                        "</form>");
+            } else {
+                out.println("<p> you don't have any duplicate video files in your drive </p>");
+            }
+            out.println("</div>");
+        } else {
+            isServletNeedRefresh = true;
+            String loadingGif = "loading.gif";
+            out.println("<h3>Loading your results...please wait...</h3>");
+            out.println("<img src='" + loadingGif + "' style='max-height:150px;max-width:150px;align:middle;'></img>");
+
+        }
+        if (isServletNeedRefresh)
+            response.setIntHeader("Refresh", 5);
+        out.println("</div></body>");
+        out.println("</html>");
+        out.close();
+    }
+}
